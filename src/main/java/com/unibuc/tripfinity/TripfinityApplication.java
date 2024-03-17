d50c7e1bc4a4d24b224da5392e015abc66ec2416
anabiancachiricu <62220976+anabiancachiricu@users.noreply.github.com>
2024-03-17 12:55:14 +0200

    Initial commit

---
+++ src/main/java/com/unibuc/tripfinity/TripfinityApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.tripfinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripfinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unibuc/tripfinity/TripfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibuc.tripfinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripfinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
