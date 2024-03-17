8743bc767d3dda12e37092c67a965ad6e894f19a
anabiancachiricu <62220976+anabiancachiricu@users.noreply.github.com>
2024-03-17 14:13:52 +0200

    First try on jwt auth

---
+++ src/main/java/com/unibuc/tripfinity/model/UserInfo.java
@@ -0,0 +1,30 @@
+package com.unibuc.tripfinity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String roles;
+    private LocalDate birthDate;
+
+}
---
+++ src/main/java/com/unibuc/tripfinity/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.unibuc.tripfinity.repository;
+
+import com.unibuc.tripfinity.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByUsername(String username);
+}
